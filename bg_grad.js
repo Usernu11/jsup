831661e319b4a93bde061a297ec79e55549e2223
Bohdan <web.dev.firefly@gmail.com>
2022-06-26 16:07:24 +0300

    finished 1 exercise of function

---
+++ bg_grad.js
@@ -0,0 +1,23 @@
+const hexidemicalNumber = [] // an empty array for hexidemical numbers
+for (let i = 0; i <= 5; i++) {
+    let shortRandomNumber = Math.ceil(Math.random() * 3) // generate a random num 1-3
+    let randomNumberOfWords = Math.ceil(Math.random() + 96) + Math.ceil(Math.random() * 5) // generate a random num 97-103
+    let randomNumber = Math.ceil(Math.random() + 48) + Math.ceil(Math.random() * 8) // generate a random num 48-57
+    let randomAFWord = String.fromCharCode(randomNumberOfWords)  // return a word from A-F alphabet by id
+    let random09Number = String.fromCharCode(randomNumber) // return a number 0-9 by id
+    if (shortRandomNumber === 1) {
+        hexidemicalNumber.push(randomAFWord) // adding a random word from the A-F alphabet to the array
+    } else if (shortRandomNumber === 2) {
+        hexidemicalNumber.push(random09Number) // adding a random number 0-9 to the array
+    } else {
+        hexidemicalNumber.push(randomAFWord.toUpperCase()) // adding a random word with upper case from the A-F alphabet to the array
+    }
+}
+
+const rgbColor = []
+for (let i = 0; i <= 2; i++) {
+    let randomNumber = Math.round(Math.random() * 255) // generate a ranodm number 0-255
+    rgbColor.push(randomNumber) // adding a random number to the array
+}
+
+document.getElementById('body').style.backgroundImage = `linear-gradient(90deg, #${hexidemicalNumber.toString().replace(/,/g, '')}, rgb(${rgbColor.toString()}))` // using the results of (hexidemical color + rbg color) above for randomly changing background-color of main element 'body' 😍
---
+++ day7_functions.js
@@ -0,0 +1,17 @@
+// Level 1
+// ex1
+function fullName (firstName, lastName) { // created a function with 2 params
+    console.log(`Your full name: ${firstName} ${lastName}`) // print the result of both params (first name and last name)
+}
+
+fullName('Oscar', 'Grand') // calling the function
+
+// ex1 ⚡ (with promt - ⚠ for testing the function → uncomment the following 2 lines of promts (10-11 and 17 lines) which markered like ⭕)
+// let firstName = prompt('Enter your first name') // create a promt for universal using the fullName function ⭕
+// let lastName = prompt('Enter your last name') // create a promt with the same goal ⭕
+
+function fullName2 (firstName, lastName) { // create the same function, but with another name of the function with 2 params for our promts
+    console.log(`Your full name: ${firstName} ${lastName}`) // add the values from promts for printing
+}
+
+// fullName2(firstName, lastName) // calling the function ⭕
