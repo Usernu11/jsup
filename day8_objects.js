8d34913c1086c8e15ea109411f9ce95bcc1d25ca
Bohdan <system.bla.16@gmail.com>
2022-07-24 00:46:11 +0300

    started 2nd ex of level 3

--- day8_objects.js
+++ day8_objects.js
@@ -245,3 +245,106 @@ const personAccount = {
   }
 }
 console.log(personAccount.accoutInfo())
+
+// ex2
+// the 2 following arrays are from course data 📜
+const users2 = [
+  {
+    _id: 'ab12ex',
+    username: 'Alex',
+    email: 'alex@alex.com',
+    password: '123123',
+    createdAt:'08/01/2020 9:00 AM',
+    isLoggedIn: false
+  },
+  {
+    _id: 'fg12cy',
+    username: 'Asab',
+    email: 'asab@asab.com',
+    password: '123456',
+    createdAt:'08/01/2020 9:30 AM',
+    isLoggedIn: true
+  },
+  {
+    _id: 'zwf8md',
+    username: 'Brook',
+    email: 'brook@brook.com',
+    password: '123111',
+    createdAt:'08/01/2020 9:45 AM',
+    isLoggedIn: true
+  },
+  {
+    _id: 'eefamr',
+    username: 'Martha',
+    email: 'martha@martha.com',
+    password: '123222',
+    createdAt:'08/01/2020 9:50 AM',
+    isLoggedIn: false
+  },
+  {
+    _id: 'ghderc',
+    username: 'Thomas',
+    email: 'thomas@thomas.com',
+    password: '123333',
+    createdAt:'08/01/2020 10:00 AM',
+    isLoggedIn: false
+  }
+];
+
+const products = [
+  {
+    _id: 'eedfcf',
+    name: 'mobile phone',
+    description: 'Huawei Honor',
+    price: 200,
+    ratings: [
+      { userId: 'fg12cy', rate: 5 },
+      { userId: 'zwf8md', rate: 4.5 }
+    ],
+    likes: []
+  },
+  {
+    _id: 'aegfal',
+    name: 'Laptop',
+    description: 'MacPro: System Darwin',
+    price: 2500,
+    ratings: [],
+    likes: ['fg12cy']
+  },
+  {
+    _id: 'hedfcg',
+    name: 'TV',
+    description: 'Smart TV:Procaster',
+    price: 400,
+    ratings: [{ userId: 'fg12cy', rate: 5 }],
+    likes: ['fg12cy']
+  }
+]
+
+// my code goes here
+console.log('')
+const signUp = (name,pass) => {
+  const usersCount = users2.length - 1
+  for (let i = 0; i <= usersCount; i++) {
+    let tempUserInfo = Object.values(users2[i]) // all info about each user
+    if (name === tempUserInfo[1]) { // checks name
+      if (pass === tempUserInfo[3]) { // checks pass
+        return `❌ You're already have an account.`
+      } else {
+        return `❌ Wrong password.`
+      }
+    } else {
+      users2.push({
+        _id: '🎲 6 nums or words',
+        username: name,
+        email: `${name.toLowerCase()}@${name.toLowerCase()}.com`,
+        password: pass,
+        createdAt: `time`,
+        isLoggedIn: true
+      })
+      return `✅ You have been added.`
+    }
+  }
+}
+console.log(signUp('Thomas','123333'))
+console.log(users2)
