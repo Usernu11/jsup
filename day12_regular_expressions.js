c0ed3af1a64025997c968606dade6f84707104f5
Bohdan <system.bla.16@gmail.com>
2023-02-13 01:17:58 +0200

    changed 1th ex of level 1

--- day12_regular_expressions.js
+++ day12_regular_expressions.js
@@ -7,3 +7,26 @@ let AllNumbers = str.match(findAllNumbers)
 let calc = (+AllNumbers[0] * 12) + (+AllNumbers[2] * 12) + +AllNumbers[1]
 
 console.log(calc)
+
+const moneyPerMonth = []
+const moneyPerYear = []
+for (let i = 0; i <= AllNumbers.length - 1; i++) {
+    if (str.match(new RegExp(AllNumbers[i] + ".*month"))) {
+        moneyPerMonth.push(AllNumbers[i])
+    }
+    if (str.match(new RegExp(AllNumbers[i] + ".*annual"))) {
+        moneyPerYear.push(AllNumbers[i])
+    }
+}
+
+console.log(`money per Month and Year: ${moneyPerMonth, moneyPerYear}`) // there is no tha last one money 5500 (how to add it?)
+
+// for (let monthSallary of moneyPerMonth) {
+//     monthSallary * 12
+// }
+
+// console.log(`calculated money per Month: ${moneyPerMonth}`)
+
+// let calc2 = moneyPerMonth.reduce((acc,cur) => acc + cur) + moneyPerYear
+
+// console.log(calc2)
