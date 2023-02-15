bd0fff8b64665c24982ee8db224b4ab345b1ad4b
Bohdan <system.bla.16@gmail.com>
2023-02-16 00:34:21 +0200

    wrote a plan for solving ex2 of level 1

--- day12_regular_expressions.js
+++ day12_regular_expressions.js
@@ -8,3 +8,17 @@ let calc = (+allNumbers[0] * 12) + (+allNumbers[2] * 12) + +allNumbers[1]
 console.log(allNumbers)
 
 console.log(`The amount of the annual sallary: $${calc}`)
+
+// ex2
+const points = ['-1', '2', '-4', '-3', '-1', '0', '4', '8']
+// 1. find neg nums using regex
+// 2. find pos nums using regex
+// 3. sort them
+// 4. concat them
+
+// 5. find max and min nums
+// 6. calculate the disctance
+
+console.log()
+// const sortedPoints =  [-4, -3, -1, -1, 0, 2, 4, 8]
+// distance = 12
