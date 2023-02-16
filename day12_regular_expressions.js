0f859b58bc529d4190c090a80d69eedf32ec7dce
Bohdan <system.bla.16@gmail.com>
2023-02-17 00:35:35 +0200

    finished ex2 of level 1

--- day12_regular_expressions.js
+++ day12_regular_expressions.js
@@ -18,9 +18,23 @@ const points = ['-1', '2', '-4', '-3', '-1', '0', '4', '8']
 
 // 5. find max and min nums
 // 6. calculate the disctance
-const negNumsPattern = /-\d+/g
-const posNumsPattern = /(?<!-)\b\d+\b/g
 
-console.log(points.toString().match(posNumsPattern))
-// const sortedPoints =  [-4, -3, -1, -1, 0, 2, 4, 8]
-// distance = 12
+// const sortedPoints =  [-4, -3, -1, -1, 0, 2, 4, 8] -> result
+// distance = 12 -> result
+
+const negNumsPattern = /-\d+/g              // 1
+const posNumsPattern = /(?<!-)\b\d+\b/g     // 2
+
+const negNums = points.toString().match(negNumsPattern).sort().reverse() // 3
+const posNums = points.toString().match(posNumsPattern).sort() // 3
+
+const sortedNums = negNums.concat(posNums)  // 4 
+
+const findMin = Math.min(...sortedNums)     // 5
+const findMax = Math.max(...sortedNums)     // 5
+
+let calcDistance = Math.abs(findMin) + findMax  // 6
+
+console.log(calcDistance) // 6
+
+// ex3
