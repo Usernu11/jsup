f7ee9fd465d2aadc019fb6d12b135e4bc87be350
Bohdan <system.bla.16@gmail.com>
2023-02-22 17:41:47 +0200

    minor changes

--- day12_regular_expressions.js
+++ day12_regular_expressions.js
@@ -123,5 +123,6 @@ console.log('')
 
 // level 3
 // ex1
+// improved a little bit previous function for the task
 const sentence = `%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?`
 tenMostFrequentWords(sentence, 10)
