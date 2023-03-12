d7c1951ca1bd2627a8f14d638246f58eacafadc8
Bohdan <system.bla.16@gmail.com>
2023-03-12 02:15:29 +0200

    finished day20

---
+++ day20_writing_clean_codes.js
@@ -0,0 +1,10 @@
+// Examples
+// 1. Airbnb style guide
+// https://github.com/airbnb/javascript
+
+// 2. Standart JS Style Guide
+// https://standardjs.com/
+
+// 3. Google Style Guide
+// https://google.github.io/styleguide/jsguide.html
+
