d6ecedf10a50fbc3d5fbb7c7c4921b097e4d070e
Bohdan <system.bla.16@gmail.com>
2023-03-13 02:20:30 +0200

    finished ex1 of level 1

--- day21_dom.js
+++ day21_dom.js
@@ -1,37 +1,12 @@
-// Theory 📄 ↓
-// const allelements = document.querySelectorAll('div')
-
-// console.log(allelements)
-// console.log(allelements.length)
-
-// for (let i = 0; i < allelements.length; i++) {
-//     console.log(allelements[i]) // prints each elements in the HTMLCollection
-// }
-
-// allelements[3].className = 'test-js'
-// allelements[3].setAttribute('class', 'testorio')
-
-// allelements[3].classList.add('js')
-// allelements[3].classList.remove('testorio')
-
-// allelements[0].textContent = '🤖👾👽'
-
-// const list = `
-// <li>30DaysOfPython Challenge Done</li>
-// <li>30DaysOfJavaScript Challenge Ongoing</li>
-// <li>30DaysOfReact Challenge Coming</li>
-// <li>30DaysOfFullStack Challenge Coming</li>
-// <li>30DaysOfDataAnalysis Challenge Coming</li>
-// <li>30DaysOfReactNative Challenge Coming</li>
-// <li>30DaysOfMachineLearning Challenge Coming</li>`
-
-// const ul = document.querySelectorAll('ul')
-// ul.innerHTML = list
-// ul.innerHTML = ''
-
-// Practice ↓
 // Level 1
 // ex1
+const bodyAccess = document.querySelector('body')
+
+for (let i = 0; i < 4; i++) {   // I used other way to solve the task, because I already had some important elements on my page.
+    const newPTag = document.createElement('p')
+    bodyAccess.appendChild(newPTag)
+}
+
 // ex2
 // ex3
 // ex4
