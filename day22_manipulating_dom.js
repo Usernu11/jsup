3f2335c6ef93adffe14a7331e9cdd604ce5af062
Bohdan <system.bla.16@gmail.com>
2023-03-20 07:42:59 +0200

    optimized code of 1ex of level 1

--- day22_manipulating_dom.js
+++ day22_manipulating_dom.js
@@ -4,46 +4,57 @@ const wrapper = document.createElement('div')
 wrapper.className = 'wrapper'
 document.querySelector('body').appendChild(wrapper)
 
-wrapper.style.width = '100%'
-wrapper.style.background = 'khaki'
-wrapper.style.height = 'auto'
-wrapper.style.display = 'flex'
-wrapper.style.flexWrap = 'wrap'
-wrapper.style.justifyContent = 'flex-start'
-wrapper.style.gap = '10px'
-wrapper.style.padding = '15px'
+const styles = {
+  wrapper: {
+    width: '100%',
+    background: 'khaki',
+    height: 'auto',
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'flex-start',
+    gap: '10px',
+    padding: '15px',    
+  },
+  numItem: {
+    width: '100px',
+    height: '30px',
+    border: '2px dashed black',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+}
 
-const isPrime = num => {    // a finction for checking prime numbers
-    for(let i = 2, s = Math.sqrt(num); i <= s; i++) {
-        if(num % i === 0) return false;
-    }
-    return num > 1;
+Object.assign(wrapper.style, styles.wrapper) // applying styles for wrapper
+
+const isPrime = num => {        // a function for checking prime numbers
+  for (let i = 2, s = Math.sqrt(num); i <= s; i++) {
+    if (num % i === 0) return false
+  }
+  return num > 1
 }
 
 for (let i = 0; i <= 100; i++) {
-    let numItem = document.createElement('div')
-    numItem.className = 'num-item'
-    numItem.id = `item-${i}`
-    numItem.textContent = `num ${i}`
-    numItem.style.width = '100px'
-    numItem.style.height = '30px'
-    numItem.style.border = '2px dashed black'
-    numItem.style.display = 'flex'
-    numItem.style.alignItems = 'center'
-    numItem.style.justifyContent = 'center'
+  let numItem = document.createElement('div')
+  numItem.className = 'num-item'
+  numItem.id = `item-${i}`
+  numItem.textContent = `num ${i}`
 
-    if (i % 2) {    // for odd nums
-        numItem.style.background = 'gold'
-    } else {
-        numItem.style.background = 'darkseagreen'
-    }
+  Object.assign(numItem.style, styles.numItem) // applying styles for num items
 
-    if (isPrime(i)) {   // check if the number is prime
-        numItem.style.background = 'salmon'
-    }
+  if (i % 2) {    // for odd nums
+    numItem.style.background = 'gold'
+  } else {
+    numItem.style.background = 'darkseagreen'
+  }
 
-    document.querySelector('.wrapper').appendChild(numItem)
+  if (isPrime(i)) {   // check if the number is prime
+    numItem.style.background = 'salmon'
+  }
+
+  wrapper.appendChild(numItem) // append child to wrapper
 }
+
 
 // Level 2
 // ex1
