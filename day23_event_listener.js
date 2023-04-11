1254fe82330d057d13efbc606dc90f48563a8339
Bohdan <system.bla.16@gmail.com>
2023-04-11 05:04:48 +0300

    added num-gen function

--- day23_event_listener.js
+++ day23_event_listener.js
@@ -29,16 +29,74 @@ button.style.border = 'none'
 button.style.padding = '15px'
 button.style.cursor = 'pointer'
 
+const isPrime = num => {        // a function for checking prime numbers
+    for (let i = 2, s = Math.sqrt(num); i <= s; i++) {
+      if (num % i === 0) return false
+    }
+    return num > 1
+}
+
+const numGen = (count) => {
+    const numsWrapper = document.createElement('div')
+    // numsWrapper.style.outline = '2px dashed black'
+    numsWrapper.style.display = 'flex'
+    numsWrapper.style.flexWrap = 'wrap'
+    numsWrapper.style.width = '50%'
+    wrapper.appendChild(numsWrapper)
+
+    for (let i = 0; i < +count + 1; i++) {
+        const newNumEl = document.createElement('div')
+        newNumEl.textContent = `${i}`
+        newNumEl.style.width = '80px'
+        newNumEl.style.height = '60px'
+        // newNumEl.style.outline = '2px dashed black'
+        newNumEl.style.margin = '5px'
+        newNumEl.style.display = 'flex'
+        newNumEl.style.alignItems = 'center'
+        newNumEl.style.justifyContent = 'center'
+        newNumEl.style.fontSize = '40px'
+
+        if (i % 2) {    // for odd nums
+            newNumEl.style.background = 'gold'
+        } else {
+            newNumEl.style.background = 'darkseagreen'
+        }
+        
+        if (isPrime(i)) {   // check if the number is prime
+            newNumEl.style.background = 'salmon'
+        }
+
+        numsWrapper.appendChild(newNumEl)
+    }
+}
+
 button.addEventListener('click', () => {
     const warnMess = document.createElement('div')
-    if (input.value === '') {
+    if (+input.value === '' || typeof +input.value === 'string') {
         input.insertAdjacentElement('beforebegin', warnMess)
         warnMess.style.color = 'red'
         warnMess.style.fontSize = '18px'
         warnMess.style.marginLeft = '10px'
-        warnMess.textContent = 'Enter number value in the number generation field please 👾'
+        warnMess.textContent = 'Enter NUMBER value please 👾'
     }
 
+    if (input.value !== '' && input.value !== 0) {
+        numGen(input.value)
+    }
 
+    return
 })
+
+// доп функц submit при помощи кнопки энтер
+// до функц при ген цифр сделать создание элементов одним за другим видимым очень быстро, но не мгновенно
+
+
+// bugs
+// 1. при пустом поле или строке нажатие на кнопку создаёт доп элемент постоянно
+// 2. при введении числа предупреждение всё равно выводится на экран ✅
+// 3. при новом значении нужно убирать старые результаты (убрать дублирование и добавление элементов на стр при постоянном нажатии на кнопку)
+// 4. после генерации чисел кнопка не реаг на пустое поле
+// 5. кнопка реаг на 0 и вызывает функц ген чисел
+
+
 // ex2
