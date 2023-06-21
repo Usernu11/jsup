b567b3071577eba09cef455cb7ebeaf0194f3988
Bohdan <107311346+Usernu11@users.noreply.github.com>
2023-06-21 20:57:20 +0300

    Add files via upload
    solved fetching countries data

--- day30_fp_l1.js
+++ day30_fp_l1.js
@@ -3,54 +3,54 @@ const getRootEl = document.querySelector('.root')
 
 // JSX elements
 const title = (
-    <h1>World Countries Data</h1>
+  <h1>World Countries Data</h1>
 )
 
 const interfaceEl = (
-    <div>
-        <input type="text" placeholder="Search country. . ."></input>
+  <div>
+    <input type="text" placeholder="Search country..." />
 
-        <div className="buttuns">
-            <button className="buttons__name">Name</button>
-            <button className="buttons__capital">Capital</button>
-            <button className="buttons__population">Population</button>
-            <button className="buttons__stat"></button>
-        </div>
+    <div className="buttuns">
+      <button className="buttons__name">Name</button>
+      <button className="buttons__capital">Capital</button>
+      <button className="buttons__population">Population</button>
+      <button className="buttons__stat" />
     </div>
+  </div>
 )
 
-const countries = (
-    <div className="countries">{countryCard}</div>
-)
+const fetchData = async () => {
+  try {
+    const response = await fetch('https://raw.githubusercontent.com/Asabeneh/30-Days-Of-JavaScript/master/data/countries_data.js')
+    const data = await response.text()
+    const countriesData = JSON.parse(data.slice(data.indexOf('[')))
 
-const stat = (
-    <div className="stat">
-        <div className="buttons">
-            <button className="buttons__pop"></button>
-            <button className="buttons__langs"></button>
-        </div>
-    </div>
-)
+    const countryCard = countriesData.map(country => (
+      <div className="countryCard" key={country.name}>
+        <img className="countryCard__flag" src={country.flag} alt={`country-flag-${country.name}`} />
+        <span className="countryCard__name">{country.name}</span>
+        <span className="countryCard__capital">{country.capital}</span>
+        <span className="countryCard__languages">{country.languages}</span>
+        <span className="countryCard__population">{country.population}</span>
+      </div>
+    ))
 
-const countryCard = (
-    countriesObjects.map(country => {
-        <div className="countryCard" key={country.name}>
-            <img className="countryCard__flag" src={country.flag} alt={`country-flag-${country.name}`}></img>
-            <span className="countryCard__name">{country.name}</span>
-            <span className="countryCard__capital">{country.capital}</span>
-            <span className="countryCard__languages">{country.languages}</span>
-            <span className="countryCard__population">{country.population}</span>
-        </div>
-    })
-)
+    const countries = (
+      <div className="countries">{countryCard}</div>
+    )
 
-const app = (
-    <div className="container">
+    const app = (
+      <div className="container">
         {title}
         {interfaceEl}
         {countries}
-        {stat}
-    </div>
-)
+      </div>
+    )
 
-ReactDOM.render(app, getRootEl)
+    ReactDOM.render(app, getRootEl)
+  } catch (error) {
+    console.log('Error fetching data:', error)
+  }
+}
+
+fetchData()
